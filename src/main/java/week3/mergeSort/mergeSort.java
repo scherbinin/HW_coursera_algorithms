257fbc6d32f590240991ffdfe83f5ec32d96da59
Shcherbinin, Ilya <ilya.shcherbinin@kronos.com>
2019-08-09 18:57:48 +0300

    merge sort

--- src/main/java/week3/mergeSort/mergeSort.java
+++ src/main/java/week3/mergeSort/mergeSort.java
@@ -4,32 +4,43 @@ package week3.mergeSort;
  * Created by scher on 08.08.2019.
  */
 public class mergeSort {
-    public void merge(int[] aux, int[] arr, int left, int right) {
-        int mid = (left + right) / 2;
-        int leftIndex = left;
-        int midIndex = mid;
-        int size = right - left;
 
-        for (int i = 0; i <= size; i++) {
-            if (midIndex > right) {
-                aux[i] = arr[leftIndex];
-                leftIndex++;
-                continue;
-            }
+   public void sort(Integer[] arr) {
+      sort(arr, 0, arr.length);
+   }
 
-            if (leftIndex == mid) {
-                aux[i] = arr[midIndex];
-                midIndex++;
-                continue;
-            }
+   private void sort(Integer[] arr, int left, int right) {
+      int mid = (right - left) / 2 + left;
 
-            if (arr[leftIndex] < arr[midIndex]) {
-                aux[i] = arr[leftIndex];
-                leftIndex++;
-            } else {
-                aux[i] = arr[midIndex];
-                midIndex++;
-            }
-        }
-    }
+      if (right - left > 1) {
+         sort(arr, left, mid);
+         sort(arr, mid, right);
+      }
+
+      Integer[] auxiliaryArr = new Integer[right - left];
+      merge(auxiliaryArr, arr, left, right, mid);
+
+      int j = 0;
+
+      for (int i = 0; i < auxiliaryArr.length; i++) {
+         arr[left + i] = auxiliaryArr[i];
+      }
+   }
+
+   public void merge(Integer[] aux, Integer[] arr, Integer left, Integer right, int mid) {
+      int leftIndex = left;
+      int midIndex = mid;
+
+      for (int i = 0; i < right - left; i++) {
+         if (midIndex == right) {
+            aux[i] = arr[leftIndex++];
+         } else if (leftIndex == mid) {
+            aux[i] = arr[midIndex++];
+         } else if (arr[leftIndex] < arr[midIndex]) {
+            aux[i] = arr[leftIndex++];
+         } else {
+            aux[i] = arr[midIndex++];
+         }
+      }
+   }
 }
--- src/test/java/week3/mergeSort/mergeSortTest.java
+++ src/test/java/week3/mergeSort/mergeSortTest.java
@@ -1,18 +1,38 @@
 package week3.mergeSort;
 
 import org.junit.Test;
+import week2.simpleSortings.InsertionSort;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by scher on 09.08.2019.
  */
 public class mergeSortTest {
     @Test
-    public void testMergingOfTwoSortedArrays() {
-        int[] arr = {-1,2,3,5,6,-2,1,4,5, 10, 11, 99};
-        int[] rez = new int[arr.length];
+    public void testMergingOfTwoSortedArrays2() {
+        Integer[] arr = {-1,2, 3,-2,1,10,12,};
+        Integer[] rez = new Integer[arr.length];
 
-        new mergeSort().merge(rez, arr, 0, arr.length-1);
+        new mergeSort().merge(rez, arr, 0, arr.length, 3);
 
-        int i =0;
     }
+
+    @Test
+    public void testAscendingSorting() {
+        List<Integer> initialData = Arrays.asList(2, 7, 1, 0, 3, 12, 434, -99871, 4, 4, 6, 5, -9,11,-99);
+        Integer[] actual = new Integer[initialData.size()];
+        Integer[] expected = new Integer[initialData.size()];
+
+        initialData.toArray(actual);
+        Arrays.sort(initialData.toArray(expected));
+
+        new mergeSort().sort(actual);
+
+        assertArrayEquals(actual, expected);
+    }
+
 }
